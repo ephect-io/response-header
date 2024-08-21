6b5bb58fd9c7e3f1f6c19a3ce1ae99a26a01fb0c
dpjb <dpjb@hotmail.fr>
2024-08-21 17:48:59 +0200

    Set Plugin::class as entrypoint class for plugins.

--- config/manifest.php
+++ config/manifest.php
@@ -8,7 +8,7 @@
 return [
     "tag" => "",
     "name" => "ephect-io/response-header",
-    "entrypoint" => null,
+    "entrypoint" => \Ephect\Forms\Components\Plugin::class,
     "templates" => "!",
     "description" => "Ephect framework component that sends custom response headers.",
     "version" => "0.7.2",
